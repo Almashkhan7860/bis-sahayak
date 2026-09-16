0f8ceb58e21f6258e7fce5d60b0d3f14ef078cfe
Almash Khan <almashpinjari807@gmail.com>
2026-09-16 19:18:08 +0530

    Fix FastAPI CORS credential conflict

--- Main/bis_sahayak_project/backend/main.py
+++ Main/bis_sahayak_project/backend/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:8000"
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
